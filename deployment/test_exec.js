ab8b04f921962333acf6230efa83408757e60490
WeiFoo <wfu@ncsu.edu>
2016-11-07 21:06:00 -0500

    test

--- deployment/test_exec.js
+++ deployment/test_exec.js
@@ -1,9 +1,26 @@
 var exec = require('child_process').exec;
-exec("ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml", function(err){
+
+exec("export ANSIBLE_HOST_KEY_CHECKING=False",function(err){
   	if (err instanceof Error)
       throw err;
     if( err )
     {
       console.error( err );
     }
-});
+  });
+
+
+const spawn = require( 'child_process' ).spawnSync,
+     ls = spawn( 'ansible-playbook', [ '-i', 'inventory_product','product.yml' ] );
+
+console.log( `stderr: ${ls.stderr.toString()}` );
+console.log( `stdout: ${ls.stdout.toString()}` );
+// var Ansible = require('node-ansible')
+// exec("ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml", function(err){
+//   	if (err instanceof Error)
+//       throw err;
+//     if( err )
+//     {
+//       console.error( err );
+//     }
+// });
