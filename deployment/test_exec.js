a57a7f09fa3bf0a3eb47a0c64ee1e6c57ccfe3ec
WeiFoo <wfu@ncsu.edu>
2016-11-07 21:09:11 -0500

    up

--- deployment/test_exec.js
+++ deployment/test_exec.js
@@ -12,8 +12,8 @@ exec("export ANSIBLE_HOST_KEY_CHECKING=False",function(err){
 
 const spawn = require( 'child_process' ).spawnSync,
      ls = spawn( 'ansible-playbook', [ '-i', '~/proxy/Milestone3/deployment/inventory_product','~/proxy/Milestone3/deployment/product.yml' ] );
-console.log( `stderr: ${ls.stderr.toString()}` );
-console.log( `stdout: ${ls.stdout.toString()}` );
+// console.log( `stderr: ${ls.stderr.toString()}` );
+// console.log( `stdout: ${ls.stdout.toString()}` );
 // var Ansible = require('node-ansible')
 // exec("ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml", function(err){
 //   	if (err instanceof Error)
