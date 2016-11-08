faa0d32347cefb067a284f18211a21b7d872e821
WeiFoo <wfu@ncsu.edu>
2016-11-08 01:17:53 -0500

    update

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -4,10 +4,13 @@ ip=$(ip route get 1 | awk '{print $NF;exit}')
 while :
 do
     cpu=$(python cpu.py)
+    echo $cpu
+
     if [ $cpu -gt 50 ]; then
         echo The CPU usage is high. $cpu%  | mail -s "High CPU usage for $sysname at ip $ip" wfu@ncsu.edu
     fi
     mem=$(python memory.py)
+    echo $mem
     if [ $mem -gt 50 ]; then
         echo The Memory usage is high. $mem% | mail -s "High memory usage for $sysname at ip $ip" wfu@ncsu.edu
     fi
@@ -16,6 +19,7 @@ do
         echo Proxy server has monitorred high traffic and a new app server is being auomatically provisioned
         node provision_newProductServer.js product
         sleep 10s
+        export ansible_host_key_checking=False
         ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml
         cd ../monitor
     fi
