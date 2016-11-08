30d1afce1cf2f313f9dad20b03d4683e6b952739
WeiFoo <wfu@ncsu.edu>
2016-11-07 22:28:24 -0500

    update

--- deployment/run_ansible.sh
+++ deployment/run_ansible.sh
@@ -1,3 +1,4 @@
 # /bin/bash
 
-ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml
+# ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml
+ansible-playbook -i inventory_product product.yml
