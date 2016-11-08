8afc2bd6840549ff6670e9a305654397498db0b4
WeiFoo <wfu@ncsu.edu>
2016-11-07 21:13:36 -0500

    up

---
+++ deployment/run_ansible.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml
--- deployment/test_exec.js
+++ deployment/test_exec.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec("export ANSIBLE_HOST_KEY_CHECKING=False",function(err){
+exec("./run_ansible",function(err){
   	if (err instanceof Error)
       throw err;
     if( err )
@@ -8,18 +8,3 @@ exec("export ANSIBLE_HOST_KEY_CHECKING=False",function(err){
       console.error( err );
     }
   });
-
-
-const spawn = require( 'child_process' ).spawnSync,
-     ls = spawn( 'ansible-playbook', [ '-i', '~/proxy/Milestone3/deployment/inventory_product','~/proxy/Milestone3/deployment/product.yml' ] );
-// console.log( `stderr: ${ls.stderr.toString()}` );
-// console.log( `stdout: ${ls.stdout.toString()}` );
-// var Ansible = require('node-ansible')
-// exec("ansible-playbook -i ~/proxy/Milestone3/deployment/inventory_product ~/proxy/Milestone3/deployment/product.yml", function(err){
-//   	if (err instanceof Error)
-//       throw err;
-//     if( err )
-//     {
-//       console.error( err );
-//     }
-// });
