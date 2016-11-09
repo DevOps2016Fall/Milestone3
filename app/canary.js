39443d188787ac944d6e13f68c5c656c36c98545
WeiFoo <wfu@ncsu.edu>
2016-11-09 15:39:12 -0500

    canary alert

---
+++ app/canary.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+var redis = require('redis');
+var redis_ip, redis_port;
+var redis_info = fs.readFileSync(path.resolve(__dirname, './redis_server.json'));
+try {
+    redisServer = JSON.parse(redis_info);
+    redis_ip = redisServer.redis_ip;
+    redis_port = redisServer.redis_port;
+}
+catch (err) {
+    console.log('parsing redis_server.json failed!');
+    console.log(err);
+}
+var client = redis.createClient(redis_port,redis_ip, {});
+var alert = process.argv[2];
+client.set('alert', alert);
